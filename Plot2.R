764ac06902a8b6eea1968e22d4973c2b97669f86
Malini <49319904+malinivijaya@users.noreply.github.com>
2019-04-28 00:35:59 -0500

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,12 @@
+baltimore <- NEI %>% 
+  filter(fips == "24510") %>% 
+  group_by(year) %>% 
+  summarize(Annual.Total = sum(Emissions));
+baltimore.pts <- pretty(baltimore$Annual.Total/1000);
+plot(baltimore$year, baltimore$Annual.Total/1000, type = "l", lwd = 2, axes = FALSE,
+     xlab = "Year", 
+     ylab = expression("Total Tons of PM"[2.5]*" Emissions"), 
+     main = expression("Total Tons of PM"[2.5]*" Emissions in Baltimore"));
+axis(1, at = c(1999,2002,2005,2008))
+axis(2, at = baltimore.pts, labels = paste(baltimore.pts, "K", sep = ""));
+box()
